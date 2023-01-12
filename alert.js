b1813faefcc362b738b47a5cef56771c3661b31b
seba.developer <cmoralesaroca@gmail.com>
2023-01-12 19:45:15 -0300

    segundo commit del proyecto

--- alert.js
+++ alert.js
@@ -1,1 +1,10 @@
-console.log("hello")
+console.log("hello");
+
+const containerNext = document.getElementById ("containerNext");
+const next = document.getElementById("next");
+
+next.addEventListener ("click", function () {
+    this.innerHTML = "Ckick for Next Color"
+    containerNext.style.backgroundColor = "red"
+    console.log("click next color")
+});
